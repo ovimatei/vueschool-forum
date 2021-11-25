2ed808e07d969bdcfea4466dcd7d8f1cf83f28aa
Ovidiu Matei <ovidiu.matei@ascentcore.com>
2021-11-25 09:44:01 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-full">
+    <h1>Welcome to the forum</h1>
+    <ThreadList :threads="Object.values(threads)" />
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+import ThreadList from './ThreadList'
+
+console.log(sourceData)
+export default {
+  name: 'HelloWorld',
+  components: {ThreadList},
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>
---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="thread">
+    <div>
+      <p>
+        <a href="#">{{ thread.title }}</a>
+      </p>
+      <p class="text-faded text-xsmall">
+        By <a href="#">Joseph Kerr</a>, {{ thread.publishedAt }}.
+      </p>
+    </div>
+
+    <div class="activity">
+      <p class="replies-count">
+        {{ repliesCount }} replies
+      </p>
+
+      <!--      <img class="avatar-medium" src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png" alt="">-->
+
+      <!--      <div>-->
+      <!--        <p class="text-xsmall">-->
+      <!--          <a href="profile.html">Bruce Wayne</a>-->
+      <!--        </p>-->
+      <!--        <p class="text-xsmall text-faded">2 hours ago</p>-->
+      <!--      </div>-->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    thread: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+    repliesCount () {
+      return Object.keys(this.thread.posts).length - 1
+    }
+  }
+}
+</script>
