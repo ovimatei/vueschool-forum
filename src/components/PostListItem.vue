d5a0d586cfe6a87d4f3b95763ba3f525e14b90aa
Ovidiu Matei <ovidiu.matei@ascentcore.com>
2021-11-28 10:57:35 +0200

    Human friendly dates

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -12,14 +12,18 @@
         <p>{{ post.text }}</p>
       </div>
     </div>
-    <div class="post-date text-faded">
-      {{ post.publishedAt }}
+    <div
+      class="post-date text-faded"
+      :title="post.publishedAt | humanFriendlyDate"
+    >
+      {{ post.publishedAt | diffForHumans }}
     </div>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
+import moment from 'moment'
 
 export default {
   name: 'PostListItem',
@@ -35,6 +39,14 @@ export default {
     },
     userPostsCount () {
       return Object.keys(this.user.posts).length
+    }
+  },
+  filters: {
+    humanFriendlyDate (date) {
+      return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
+    },
+    diffForHumans (date) {
+      return moment.unix(date).fromNow()
     }
   }
 }
