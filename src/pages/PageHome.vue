84aa5e42a649e315bbaf6981a42848a3bd309bae
Ovidiu Matei <ovidiu.matei@ascentcore.com>
2021-11-30 17:24:32 +0200

    Forum components

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="col-full">
     <h1>Welcome to the forum</h1>
-    <ThreadList :threads="Object.values(threads)"/>
+    <ForumList :forums="Object.values(forums)"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
 import ThreadList from '@/components/ThreadList'
+import ForumList from '../components/ForumList'
 
 console.log(sourceData)
 export default {
   name: 'Home',
-  components: {ThreadList},
+  components: {ForumList, ThreadList},
   data () {
     return {
       threads: sourceData.threads,
       posts: sourceData.posts,
-      users: sourceData.users
+      users: sourceData.users,
+      forums: sourceData.forums
     }
   }
 }
